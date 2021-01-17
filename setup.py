f471ae6d8525320cff1f6fc9538f9ebab01d62ce
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-01-16 22:06:24 -0300

    enviado pacote com alteraçoes no build

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fast_orm",
+    version="0.1.0",
+    author="Oscar da Silva",
+    author_email="oscarkaka222@gmail.com",
+    description="Um pacote simples para realizar operações no banco",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OscarSilvaOfficial/easy_sql",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'six>=1.15.0',
+        'mysqlclient>=2.0.3',
+        'mysql-connector-python>=8.0.22',
+        'mysql>=0.0.2'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
