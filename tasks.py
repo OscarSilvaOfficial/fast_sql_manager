031ca02266a6fcd6cf31f9fb2567f26f4b88b76c
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-01-17 16:32:52 -0300

    Enviado alteraçoes de scripts invoke

--- tasks.py
+++ tasks.py
@@ -1,9 +1,23 @@
 from invoke import task
 
+
+@task
+def build(c, docs=False):
+    c.run('python setup.py sdist bdist_wheel')
+    
+@task
+def push(c, docs=False):
+    c.run('git add .')
+    msg = input('Escreva a mensagem de commit: \n')
+    c.run('git commit -m"{0}"'.format(msg))
+    c.run('git push origin master')
+
+@task
+def uploadPypi(c, docs=False):
+    c.run('twine upload --skip-existing dist/*')
+
 @task
 def deploy(c, docs=False):
-    c.run('python setup.py sdist bdist_wheel')
-    c.run('git add .')
-    c.run('git commit -m"atualização da versão"')
-    c.run('git push origin master')
-    c.run('twine upload --skip-existing dist/*')
+    build()
+    push()
+    uploadPypi()
