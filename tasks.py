b6b56d1e60e874cfe62606e621bc71647a6a5a4b
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-01-17 22:07:44 -0300

    Criando Script de Automaçao para alteraçao de versao

--- tasks.py
+++ tasks.py
@@ -1,10 +1,71 @@
+import re
 from invoke import task
+
+
+def changeHotFix():
+    filename = "setup.py"
+    regex = r'[0-9]?[0-9].[0-9]?[0-9].[0-9]?[0-9]'
+
+    with open(filename, 'r+') as f:
+        text = f.read()
+        subs = re.findall(regex, text)[0]  # 1.1.2
+        reg = re.findall(r'.[0-9]?[0-9]', subs)[-1]  # .2
+
+        res = re.sub(reg, f'.{str(int(reg.replace(".", ""))+1)}', subs)
+
+        text = re.sub(subs, res, text)
+        f.seek(0)
+        f.write(text)
+        f.truncate()
+
+        print(f'HotFix alterado de {subs} para {res}')
+
+
+def changeMinor():
+    filename = "setup.py"
+    regex = r'[0-9]?[0-9].[0-9]?[0-9].[0-9]?[0-9]'
+
+    with open(filename, 'r+') as f:
+        text = f.read()
+        subs = re.findall(regex, text)[0]
+        reg = r'.[0-9]?[0-9].'
+        res = re.sub(
+            reg,
+            f".{str(int(re.findall(reg, subs)[0].replace('.', ''))+1)}.",
+            subs)
+
+        text = re.sub(subs, res, text)
+        f.seek(0)
+        f.write(text)
+        f.truncate()
+
+    print(f'Minor alterado de {subs} para {res}')
+
+
+def changeMajor():
+    filename = "setup.py"
+    regex = r'[0-9]?[0-9].[0-9]?[0-9].[0-9]?[0-9]'
+
+    with open(filename, 'r+') as f:
+        text = f.read()
+        subs = re.findall(regex, text)[0]  # 1.1.2
+        reg = re.findall(r'[0-9]?[0-9].', subs)[0]  # .2
+
+        res = re.sub(reg, f'{str(int(reg.replace(".", ""))+1)}.', subs)
+
+        text = re.sub(subs, res, text)
+        f.seek(0)
+        f.write(text)
+        f.truncate()
+
+        print(f'Major alterado de {subs} para {res}')
 
 
 @task
 def build(c, docs=False):
     c.run('python setup.py sdist bdist_wheel')
-    
+
+
 @task
 def push(c, docs=False):
     c.run('git add .')
@@ -12,12 +73,27 @@ def push(c, docs=False):
     c.run('git commit -m"{0}"'.format(msg))
     c.run('git push origin master')
 
+
 @task
 def uploadPypi(c, docs=False):
     c.run('twine upload --skip-existing dist/*')
 
+
 @task
-def deploy(c, docs=False):
-    build()
-    push()
-    uploadPypi()
+def chooseManagementVersion(c, docs=False):
+    version = input('Alterar [H-Hotfix | m-minor | M-Major]: ')
+
+    if version == 'H':
+        changeHotFix()
+    if version == 'm':
+        changeMinor()
+    if version == 'M':
+        changeMajor()
+
+
+@task
+def deployToPypi(c, docs=False):
+    c.run('inv chooseManagementVersion')
+    c.run('inv build')
+    c.run('inv push')
+    c.run('inv uploadPypi')
