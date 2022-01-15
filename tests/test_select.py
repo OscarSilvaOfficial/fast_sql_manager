7c04bbebdf294b0a970fdfd24a7816abbd753d09
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-01-15 01:16:37 -0300

    Add testes

---
+++ tests/test_select.py
@@ -0,0 +1,29 @@
+from fast_sql_manager.repository import Repository
+
+db = Repository(
+  host='localhost',
+  port=3306, 
+  user='root', 
+  passwd='admin',
+)
+
+def create_tables():
+  db.create_database('teste')
+  db.create_table(
+    name='tb_teste',
+    columns={
+      'id': 'int not null primary key auto_increment',
+      'name': 'varchar(255)',
+    }
+  )
+  db.insert(
+    table_name='tb_teste',
+    table_columns=['name'],
+    insert_values=('Oscar',)
+  )
+
+
+def test_select():
+  create_tables()
+  # print(db.select_all('teste'))
+  
