b336fff50af17c154507cffeb0226c539fa1862b
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-01-18 02:04:25 -0300

    Remoção de parâmetro, reduzindo escopo de acesso

--- fast_sql_manager/abstractions/_mongo.py
+++ fast_sql_manager/abstractions/_mongo.py
@@ -12,7 +12,8 @@ class Mongo(object):
     self._conn: MongoClient = db_config.get_connection()
     self._db_name = db_name
       
-  def select_all(self, collection_name, where={}, return_data=[]):
+  def select_all(self, collection_name, where={}):
+    return_data=[]
     collection = self._conn[self._db_name][collection_name]
     for item in collection.find(where): return_data.append(item)
     return return_data
