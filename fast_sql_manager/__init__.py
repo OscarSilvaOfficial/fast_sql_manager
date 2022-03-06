04c17cffe4d99c187898c89ff006ce5b695d98cb
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-03-05 22:15:50 -0300

    Ajuste no nome do arquivo

--- fast_sql_manager/__init__.py
+++ fast_sql_manager/__init__.py
@@ -1,2 +1,3 @@
 from fast_sql_manager.implementations.mysql import MySQLRepository
 from fast_sql_manager.implementations.sqlite import SQLite
+from fast_sql_manager.implementations.postgres import Postgres
