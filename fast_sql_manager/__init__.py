f1a901470013c0495b2e853e88fa8a3b30362f20
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-03-06 22:04:57 -0300

    Correção no import

--- fast_sql_manager/__init__.py
+++ fast_sql_manager/__init__.py
@@ -1,3 +1,3 @@
 from fast_sql_manager.implementations.mysql import MySQLRepository
-from fast_sql_manager.implementations.sqlite import SQLite
-from fast_sql_manager.implementations.postgres import Postgres
+from fast_sql_manager.implementations.sqlite import SQLiteRepository
+from fast_sql_manager.implementations.postgres import PostgresRepository
